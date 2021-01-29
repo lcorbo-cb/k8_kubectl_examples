282ee041d4b8d7373045e3189df2c400c0e26b1b
lcorbo-cb <lcorbo@cloudbees.com>
2021-01-29 14:08:14 -0500

    added ingress

--- Services/kubectl_service_commands.sh
+++ Services/kubectl_service_commands.sh
@@ -1,15 +1,37 @@
-kubectl create namespace servicetest # Create a namespace for our example
+kubectl create namespace statetest # Create a namespace for our example
 
-kubectl config set-context --current --namespace=servicetest
+kubectl config set-context --current --namespace=statetest
 
-kubectl apply -f simple_service.yaml
+kubectl apply -f statefulset_example.yaml
 
-kubectl get services my-service -o wide
+kubectl get service nginx
 
-kubectl get services my-service -o yaml
+kubectl get statefulset web
 
-kubectl delete service my-service
+kubectl get pods -w -l app=nginx
+
+kubectl get pods -l app=nginx
+
+for i in 0 1; do kubectl exec "web-$i" -- sh -c 'hostname'; done
+
+kubectl run -i --tty --image busybox:1.28 dns-test --restart=Never --rm
+
+# In session
+nslookup web-0.nginx
+
+{
+  # In Terminal One
+  kubectl get pod -w -l app=nginx
+
+  # In Terminal Two
+
+  kubectl delete pod -l app=nginx
+}
+
+for i in 0 1; do kubectl exec web-$i -- sh -c 'hostname'; done
+
+kubectl delete -f statefulset_example.yaml
 
 kubectl config set-context --current --namespace=default # Set context to default as we are about to destroy the namespace
 
-kubectl delete namespace servicetest
+kubectl delete namespace statetest
